4f762fdf5972e8cb145a4cce55addaef88f77d37
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2023-07-15 17:56:04 +0530

    migrated aws-sdk to v3

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,4 +1,4 @@
-import S3 from 'aws-sdk/clients/S3';
+import { S3Client, AbortMultipartUploadCommand } from "@aws-sdk/client-s3";
 
 type UploadedObjectRes = {
   signedRequest : {
@@ -8,15 +8,15 @@ type UploadedObjectRes = {
 
 class S3Singleton {
   private static instance: S3Singleton | null = null;
-  private s3Client: S3;
+  private s3Client: S3Client;
 
   private constructor() {
-    const s3Config: S3.ClientConfiguration = {
+    const s3Config: S3Client.ClientConfiguration = {
       accessKeyId: process.env.AWS_ACCESS_KEY,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       region: process.env.AWS_REGION,
     };
-    this.s3Client = new S3(s3Config);
+    this.s3Client = new S3Client(s3Config);
   }
 
   public static getInstance(): S3Singleton {
@@ -26,7 +26,7 @@ class S3Singleton {
     return S3Singleton.instance;
   }
 
-  public async uploadObject(params: S3.PutObjectRequest) : Promise<UploadedObjectRes>{
+  public async uploadObject(params: S3Client.PutObjectRequest) : Promise<UploadedObjectRes>{
     return new Promise((resolve, reject) => {
       this.s3Client.upload(params,(err: any, data: any)=>{
         if (err) {
@@ -40,7 +40,7 @@ class S3Singleton {
     });
   }
 
-  public async getObject(params: S3.GetObjectRequest) {
+  public async getObject(params: S3Client.GetObjectRequest) {
     const data = await this.s3Client.getObject(params);
     return data
   }
