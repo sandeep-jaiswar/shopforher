b6882ebcddd830e716d757419025794ab3331ecc
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2023-07-16 11:29:54 +0530

    added dependency packages

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,20 +1,22 @@
-import { S3Client, AbortMultipartUploadCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, GetObjectCommand, PutObjectCommandInput, GetObjectCommandInput, GetObjectCommandOutput } from "@aws-sdk/client-s3";
 
 type UploadedObjectRes = {
-  signedRequest : {
-    Location : string
-  }
-}
+  signedRequest: {
+    Location: string;
+  };
+};
 
 class S3Singleton {
   private static instance: S3Singleton | null = null;
   private s3Client: S3Client;
 
   private constructor() {
-    const s3Config: S3Client.ClientConfiguration = {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION,
+    const s3Config = {
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY || "",
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+      },
+      region: process.env.AWS_REGION || "",
     };
     this.s3Client = new S3Client(s3Config);
   }
@@ -26,23 +28,24 @@ class S3Singleton {
     return S3Singleton.instance;
   }
 
-  public async uploadObject(params: S3Client.PutObjectRequest) : Promise<UploadedObjectRes>{
+  public async uploadObject(params: PutObjectCommandInput): Promise<UploadedObjectRes> {
     return new Promise((resolve, reject) => {
-      this.s3Client.upload(params,(err: any, data: any)=>{
+      this.s3Client.send(new PutObjectCommand(params), (err: any, data: any) => {
         if (err) {
           reject(err);
         }
-        const returnData = {
-          signedRequest: data
+        const returnData: UploadedObjectRes = {
+          signedRequest: {
+            Location: data.Location,
+          },
         };
         resolve(returnData);
-      })
+      });
     });
   }
 
-  public async getObject(params: S3Client.GetObjectRequest) {
-    const data = await this.s3Client.getObject(params);
-    return data
+  public async getObject(params: GetObjectCommandInput): Promise<GetObjectCommandOutput> {
+    return this.s3Client.send(new GetObjectCommand(params));
   }
 }
 
