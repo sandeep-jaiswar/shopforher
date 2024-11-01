22e77b43e855c870dca72fd069a51e6eac73d4b2
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-11-02 00:38:12 +0530

    feat: setup custom theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import "@/styles/globals.css";
+import theme from "@/lib/theme";
+import { ThemeProvider } from "@mui/material";
+import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+const App = (props: AppProps) => {
+  const { Component, pageProps } = props;
+  return (
+    <AppCacheProvider {...props}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AppCacheProvider>
+  );
 }
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,27 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from "@mui/material-nextjs/v15-pagesRouter";
 
-const Document = () => {
+const Document = (props: DocumentHeadTagsProps) => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <DocumentHeadTags {...props} />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
       </body>
     </Html>
   );
-}
+};
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
 
 export default Document;
