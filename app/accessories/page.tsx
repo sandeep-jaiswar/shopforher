6b85199be0597a64119044d27c2242056714fa81
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-11-01 23:49:55 +0530

    chore: reset

--- app/accessories/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Accessories() {
-  return (
-    <div>Accessories</div>
-  )
-}
-
-export default Accessories
--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['genericapi.s3.ap-south-1.amazonaws.com'],
-    }
-}
-
-module.exports = nextConfig
